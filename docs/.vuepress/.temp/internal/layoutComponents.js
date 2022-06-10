2f083410bb50302b07499f4dd646c1426bb630a0
0X7FFFFFFF <31471833+Int-0X7FFFFFFF@users.noreply.github.com>
2022-06-10 14:09:28 -0600

    init

--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,8 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("/Users/ass_code_root/Documents/RainBotPRoject/doc/node_modules/.pnpm/vuepress-theme-hope@2.0.0-beta.79/node_modules/vuepress-theme-hope/lib/client/layouts/404.js")),
-  "Layout": defineAsyncComponent(() => import("/Users/ass_code_root/Documents/RainBotPRoject/doc/node_modules/.pnpm/vuepress-theme-hope@2.0.0-beta.79/node_modules/vuepress-theme-hope/lib/client/layouts/Layout.js")),
-  "Slide": defineAsyncComponent(() => import("/Users/ass_code_root/Documents/RainBotPRoject/doc/node_modules/.pnpm/vuepress-theme-hope@2.0.0-beta.79/node_modules/vuepress-theme-hope/lib/client/layouts/Slide.js")),
-  "Blog": defineAsyncComponent(() => import("/Users/ass_code_root/Documents/RainBotPRoject/doc/node_modules/.pnpm/vuepress-theme-hope@2.0.0-beta.79/node_modules/vuepress-theme-hope/lib/client/module/blog/layouts/Blog.js")),
-}
--- docs/.vuepress/.temp/theme-hope/sidebar.js
+++
@@ -1,1 +0,0 @@
-export const sidebarData = {"/guide/":["disable.md","encrypt.md","page.md","markdown.md"]}
